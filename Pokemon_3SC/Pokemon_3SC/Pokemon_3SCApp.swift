21a71d8bcf93b96ac99f883dac7253a2882c387a
snuffydan <danushikadan@gmail.com>
2024-07-18 11:38:31 +0100

    Initial commit

---
+++ Pokemon_3SC/Pokemon_3SC/Pokemon_3SCApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_3SCApp.swift
+//  Pokemon_3SC
+//
+//  Created by Danushika Priyadarshani on 18/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_3SCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
