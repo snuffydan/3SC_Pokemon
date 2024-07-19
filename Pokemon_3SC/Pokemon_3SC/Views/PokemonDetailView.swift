131d628c02704bfd9d05a6b97c0489c6979688ea
snuffydan <danushikadan@gmail.com>
2024-07-19 11:48:40 +0100

    Added Pokemon detail view

---
+++ Pokemon_3SC/Pokemon_3SC/Views/PokemonDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonDetailView.swift
+//  Pokemon_3SC
+//
+//  Created by Danushika Priyadarshani on 19/07/2024.
+//
+
+import SwiftUI
+
+struct PokemonDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonDetailView()
+}
